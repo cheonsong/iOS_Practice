a264850570a0f5f83e498ce080f7835dfe7e0aab
cheonsong <qkrcjsthd@gmail.com>
2021-10-05 21:07:15 +0900

    Quote Generator

---
+++ QuotesGenerator/QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by cheonsong on 2021/10/05.
+//
+
+import Foundation
+
+struct Quote {
+    let content: String
+    let name: String
+}
--- QuotesGenerator/QuotesGenerator/ViewController.swift
+++ QuotesGenerator/QuotesGenerator/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    let quotes: [Quote] = [
+        Quote(content: "집에 가고 싶어", name: "나"),
+        Quote(content: "개발자는 은퇴하는 그 날까지 공부한다.", name: "나1"),
+        Quote(content: "1일 1커밋하자", name: "나2"),
+        Quote(content: "하루에 더도말고 네시간씩만 해보자", name: "나3"),
+        Quote(content: "꾸준히 페이스를 유지하며 성장하자", name: "나4")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapQuoateGeneratorButton(_ sender: Any) {
+        let random = Int(arc4random_uniform(5)) //0~4사이의 난수
+        let quote = quotes[random]
+        self.quoteLabel.text = quote.content
+        self.nameLabel.text = quote.name
+    }
+    
 }
 
