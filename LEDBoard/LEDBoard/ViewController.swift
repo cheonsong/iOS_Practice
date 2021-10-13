8504e320de022bac8d835e509fc4ac3fda782cd9
cheonsong <qkrcjsthd@gmail.com>
2021-10-13 22:01:55 +0900

    LED Board

--- LEDBoard/LEDBoard/ViewController.swift
+++ LEDBoard/LEDBoard/ViewController.swift
@@ -7,15 +7,32 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, LEDBoardSettingDelegate {
 
+    
     @IBOutlet weak var contentLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.contentLabel.textColor = .yellow
-        // Do any additional setup after loading the view.
+        self.contentLabel.textColor = .yellow
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let settingViewController = segue.destination as? SettingViewController {
+            settingViewController.delegate = self
+            settingViewController.ledText = self.contentLabel.text
+            settingViewController.textColor = self.contentLabel.textColor
+            settingViewController.backgroundColor = self.view.backgroundColor ?? .blue
+        }
+    }
+    
+    func changedSetting(text: String?, textColor: UIColor, backgroudColor: UIColor) {
+        if let text = text {
+            self.contentLabel.text = text
+        }
+        self.contentLabel.textColor = textColor
+        self.view.backgroundColor = backgroudColor
+    }
 
 }
 
