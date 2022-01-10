11303e055d9214229ee456fd7f5e502fb553568d
yeoboya_211221_03 <qkrcjsthd@gmail.com>
2022-01-10 10:48:36 +0900

    Bridge

---
+++ SocketEX/SocketEX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketEX
+//
+//  Created by yeoboya_211221_03 on 2022/01/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
